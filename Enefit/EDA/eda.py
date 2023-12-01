2a3208d8a06cd3ba1bb6d1ec40e3daee4c7a81a6
Runjie Zhu <damiendesolei@hotmail.com>
2023-12-01 19:24:57 +1100

    Update eda.py
    load historical_weather

--- Enefit/EDA/eda.py
+++ Enefit/EDA/eda.py
@@ -5,6 +5,7 @@
 ####Shanghai-China#####
 
 import pandas as pd 
+pd.set_option('display.max_columns', None)
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -17,9 +18,20 @@ data_path = Path('C:/Users/damie/Downloads/predict-energy-behavior-of-prosumers'
 
 train = pd.read_csv(data_path / 'train.csv', parse_dates=['datetime'])
 train.info(memory_usage='deep')
-
+train.dtypes
 train.head()
 
-for col in train.columns:
-    print(str(col) + ' has unique values of:')
-    print(set(train[col].unique()))
+# for col in train.columns:
+#     print(str(col) + ' has unique values of:')
+#     print(set(train[col].unique()))
+    
+    
+historical_weather = pd.read_csv(data_path / 'historical_weather.csv', parse_dates=['datetime'])
+historical_weather.info(memory_usage='deep')
+historical_weather.dtypes
+historical_weather.head()
+
+# for col in historical_weather.columns:
+#     print(str(col) + ' has unique values of:')
+#     print(set(historical_weather[col].unique()))
+    
