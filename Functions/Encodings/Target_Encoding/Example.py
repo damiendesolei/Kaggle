6a3d78e23ee979c219ca7bd542ef01adc4b5b7ef
Runjie Zhu <damiendesolei@hotmail.com>
2024-02-08 21:11:14 +1100

    setup target encoding folder

---
+++ Functions/Encodings/Target_Encoding/Example.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#https://www.kaggle.com/code/ryanholbrook/target-encoding
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import warnings
+
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=14,
+    titlepad=10,
+)
+warnings.filterwarnings('ignore')
+
+
+df = pd.read_csv(r"G:\kaggle\Functions\Target_Encoding\data\movielens1m.csv")
+df = df.astype(np.uint8, errors='ignore') # reduce memory footprint
+print("Number of Unique Zipcodes: {}".format(df["Zipcode"].nunique()))
+
+#split the data into train and test
+X = df.copy()
+y = X.pop('Rating')
+
+X_encode = X.sample(frac=0.25)
+y_encode = y[X_encode.index]
+X_pretrain = X.drop(X_encode.index)
+y_train = y[X_pretrain.index]
+
+
+from category_encoders import MEstimateEncoder
+
+# Create the encoder instance. Choose m to control noise.
+encoder = MEstimateEncoder(cols=["Zipcode"], m=5.0)
+
+# Fit the encoder on the encoding split.
+encoder.fit(X_encode, y_encode)
+
+# Encode the Zipcode column to create the final training data
+X_train = encoder.transform(X_pretrain)
+
+
+
+plt.figure(dpi=90)
+ax = sns.distplot(y, kde=False, norm_hist=True)
+ax = sns.kdeplot(X_train.Zipcode, color='r', ax=ax)
+ax.set_xlabel("Rating")
+ax.legend(labels=['Zipcode', 'Rating']);
