9be2a9b897531b7b3f9651a0030d04385c8ecd5f
Runjie Zhu <damiendesolei@hotmail.com>
2024-03-02 23:12:31 +1100

    Create modelling.py
    source:
    https://www.kaggle.com/code/beckpro/previous-2022-xgboost-solution-simple-submission

---
+++ March_Machine_Learning_Mania 2024/modelling.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  2 12:02:17 2024
+
+@author: zrj-desktop
+"""
+
+import os
+import re
+
+import pandas as pd
+#import polars as pl
+
+# Show all properties on display and set style
+pd.set_option('display.max_columns', None)
+DATA_PATH = 'G:/kaggle/March_Machine_Learning_Mania_2024/march-machine-learning-mania-2024/'
+
+for filename in sorted(os.listdir(DATA_PATH)):
+    print(filename)
+    
+df_seeds = pd.concat([
+    pd.read_csv(DATA_PATH + "MNCAATourneySeeds.csv"),
+    pd.read_csv(DATA_PATH + "WNCAATourneySeeds.csv"),
+], ignore_index=True)
+    
+df_seeds.head()
+
+
+df_season_results = pd.concat([
+    pd.read_csv(DATA_PATH + "MRegularSeasonCompactResults.csv"),
+    pd.read_csv(DATA_PATH + "WRegularSeasonCompactResults.csv"),
+], ignore_index=True)
+
+df_season_results
