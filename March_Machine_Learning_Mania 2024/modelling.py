a69ad9fba2c954d56003c91d143f6e957565cd65
Runjie Zhu <damiendesolei@hotmail.com>
2024-03-10 14:19:55 +1100

    Update modelling.py
    draft1

--- March_Machine_Learning_Mania 2024/modelling.py
+++ March_Machine_Learning_Mania 2024/modelling.py
@@ -9,6 +9,7 @@ import os
 import re
 
 import pandas as pd
+import numpy as np
 #import polars as pl
 
 # Show all properties on display and set style
@@ -244,3 +245,310 @@ preds_w_seeds['Pred'] = preds_w_seeds.apply(lambda x: 1 - x['Pred'] if x['Team1I
 preds_w_seeds = preds_w_seeds[['ID', 'Pred', 'Year', 'HigherSeedID', 'LowerSeedID', 'Tournament', 'HigherSeed', 'LowerSeed']]
 
 preds_w_seeds['new_ID'] = preds_w_seeds['Tournament'] + '_' + preds_w_seeds['Year'].astype(str) + '_' + preds_w_seeds['HigherSeedID'] + '_' + preds_w_seeds['LowerSeedID']
+
+
+# Function to simulate a single matchup
+def simulate_matchup(probability):
+    return np.random.rand() < probability  # Returns True if Team1 wins, False if Team2 wins
+
+#sample_submission as template for the output
+sample_submission = pd.read_csv(DATA_PATH  + "/sample_submission.csv")
+
+submission = sample_submission.copy()
+submission.rename(columns={'Team':'Winner'},inplace=True)
+
+# read in new seeds
+tourney_slots = pd.read_csv(DATA_PATH + '/MNCAATourneySlots.csv')
+#filter out tourney_slots to only include 2023
+tourney_slots = tourney_slots[tourney_slots['Season'] == 2023]
+
+tourney_slots_M = tourney_slots.copy()
+tourney_slots_M['Tournament'] = 'M'
+
+tourney_slots_W = tourney_slots.copy()
+tourney_slots_W['Tournament'] = 'W'
+
+tourney_slots = pd.concat([tourney_slots_M, tourney_slots_W])
+
+
+def create_round(slot):
+    if slot[0] != 'R':
+        return 0
+    elif slot[0] == 'R':
+        return int(slot[1])
+    
+tourney_slots['Round'] = tourney_slots['Slot'].apply(create_round)
+tourney_slots = tourney_slots[tourney_slots['Round'] != 0]
+
+preds_df = preds_w_seeds[['ID','new_ID','Year','HigherSeedID','LowerSeedID','Tournament','Pred']]
+
+preds_df
+
+
+#merge preds_df with initial_matchups to get the pred for round 1 matchup
+
+tourney_2023 = tourney_slots.copy()
+
+#find round 1 matchups
+initial_matchups = tourney_2023[tourney_2023['Round'] == 1]
+
+results = initial_matchups.merge(preds_df, left_on=['StrongSeed','WeakSeed','Season'], right_on=['HigherSeedID','LowerSeedID','Year'], how='left')
+
+results.rename(columns={'Tournament_x':'Tournament'}, inplace=True)
+#drop Touranment_y column  
+results.drop('Tournament_y', axis=1, inplace=True)
+
+
+#simulate round 1 matchups, if simulate_matchup results in True, winner is the higher seed, if False, winner is the lower seed
+results['outcome'] = results['Pred'].apply(simulate_matchup)
+
+results['Team'] = results.apply(lambda x: x['HigherSeedID'] if x['outcome'] == True else x['LowerSeedID'], axis=1)
+
+#drop duplicates from results
+results.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+results
+
+round_1_results = results[['Tournament','Slot','Team','ID']]
+
+round_1_results
+
+
+#take Team from round_1_results and merge with tourney_2023 to get the next round matchups
+round_2 = tourney_2023[tourney_2023['Round'] == 2]
+
+round_2 = round_2.merge(round_1_results, left_on=['StrongSeed','Tournament'], right_on=['Slot','Tournament'], how='left')
+#rename original columns
+round_2.rename(columns={'Slot_x':'Slot', 'Tournament_x':'Tournament'}, inplace=True)
+#drop columns from merge
+round_2.drop(['Slot_y'], axis=1, inplace=True)
+
+#drop duplicates
+round_2.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+
+round_2 = round_2.merge(round_1_results, left_on=['WeakSeed','Tournament'], right_on=['Slot','Tournament'], how='left')
+#rename original columns
+round_2.rename(columns={'Slot_x':'Slot', 'Tournament_x':'Tournament'}, inplace=True)
+#drop columns from merge
+round_2.drop(['Slot_y'], axis=1, inplace=True)
+
+#drop duplicates
+round_2.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+
+#if team_x is the lower seed, set StrongSeed to Team_x and WeakSeed to Team_y, else set StrongSeed to Team_y and WeakSeed to Team_x
+round_2['StrongSeed'] = round_2.apply(lambda x: x['Team_x'] if x['Team_x'] < x['Team_y'] else x['Team_y'], axis=1)
+round_2['WeakSeed'] = round_2.apply(lambda x: x['Team_x'] if x['Team_x'] > x['Team_y'] else x['Team_y'], axis=1)
+
+#drop Team_x and Team_y
+round_2.drop(['Team_x', 'Team_y'], axis=1, inplace=True)
+
+
+#merge round_2 with preds_df to get the pred for round 2 matchups
+round_2 = round_2.merge(preds_df, left_on=['StrongSeed','WeakSeed','Tournament'], right_on=['HigherSeedID','LowerSeedID','Tournament'], how='left')
+
+round_2['outcome'] = round_2['Pred'].apply(simulate_matchup)
+
+round_2['Team'] = round_2.apply(lambda x: x['HigherSeedID'] if x['outcome'] == True else x['LowerSeedID'], axis=1)
+
+round_2
+
+round_2_results = round_2[['Tournament','Slot','Team','ID']]
+
+round_2_results
+
+
+round_3 = tourney_2023[tourney_2023['Round'] == 3]
+
+round_3 = round_3.merge(round_2_results, left_on=['StrongSeed','Tournament'], right_on=['Slot','Tournament'], how='left')
+#rename original columns
+round_3.rename(columns={'Slot_x':'Slot', 'Tournament_x':'Tournament'}, inplace=True)
+#drop columns from merge
+round_3.drop(['Slot_y'], axis=1, inplace=True)
+
+#drop duplicates
+round_3.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+round_3 = round_3.merge(round_2_results, left_on=['WeakSeed','Tournament'], right_on=['Slot','Tournament'], how='left')
+#rename original columns
+round_3.rename(columns={'Slot_x':'Slot', 'Tournament_x':'Tournament'}, inplace=True)
+#drop columns from merge
+round_3.drop(['Slot_y'], axis=1, inplace=True)
+
+#drop duplicates
+round_3.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+
+round_3['StrongSeed'] = round_3.apply(lambda x: x['Team_x'] if x['Team_x'] < x['Team_y'] else x['Team_y'], axis=1)
+round_3['WeakSeed'] = round_3.apply(lambda x: x['Team_x'] if x['Team_x'] > x['Team_y'] else x['Team_y'], axis=1)
+
+#drop Team_x and Team_y
+round_3.drop(['Team_x', 'Team_y'], axis=1, inplace=True)
+
+
+round_3 = round_3.merge(preds_df, left_on=['StrongSeed','WeakSeed','Tournament'], right_on=['HigherSeedID','LowerSeedID','Tournament'], how='left')
+
+round_3['outcome'] = round_3['Pred'].apply(simulate_matchup)
+
+round_3['Team'] = round_3.apply(lambda x: x['HigherSeedID'] if x['outcome'] == True else x['LowerSeedID'], axis=1)
+
+round_3
+
+round_3_results = round_3[['Tournament','Slot','Team','ID']]
+round_3_results
+
+
+round_4 = tourney_2023[tourney_2023['Round'] == 4]
+
+round_4 = round_4.merge(round_3_results, left_on=['StrongSeed','Tournament'], right_on=['Slot','Tournament'], how='left')
+#rename original columns
+round_4.rename(columns={'Slot_x':'Slot', 'Tournament_x':'Tournament'}, inplace=True)
+#drop columns from merge
+round_4.drop(['Slot_y'], axis=1, inplace=True)
+
+#drop duplicates
+round_4.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+round_4 = round_4.merge(round_3_results, left_on=['WeakSeed','Tournament'], right_on=['Slot','Tournament'], how='left')
+#rename original columns
+round_4.rename(columns={'Slot_x':'Slot', 'Tournament_x':'Tournament'}, inplace=True)
+#drop columns from merge
+round_4.drop(['Slot_y'], axis=1, inplace=True)
+    
+#drop duplicates
+round_4.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+round_4['StrongSeed'] = round_4.apply(lambda x: x['Team_x'] if x['Team_x'] < x['Team_y'] else x['Team_y'], axis=1)
+round_4['WeakSeed'] = round_4.apply(lambda x: x['Team_x'] if x['Team_x'] > x['Team_y'] else x['Team_y'], axis=1)
+
+#drop Team_x and Team_y
+round_4.drop(['Team_x', 'Team_y'], axis=1, inplace=True)
+
+round_4 = round_4.merge(preds_df, left_on=['StrongSeed','WeakSeed','Tournament'], right_on=['HigherSeedID','LowerSeedID','Tournament'], how='left')
+
+round_4['outcome'] = round_4['Pred'].apply(simulate_matchup)
+
+round_4['Team'] = round_4.apply(lambda x: x['HigherSeedID'] if x['outcome'] == True else x['LowerSeedID'], axis=1)
+
+round_4
+
+round_4_results = round_4[['Tournament','Slot','Team','ID']]
+
+round_4_results
+
+
+round_5 = tourney_2023[tourney_2023['Round'] == 5]
+
+round_5 = round_5.merge(round_4_results, left_on=['StrongSeed','Tournament'], right_on=['Slot','Tournament'], how='left')
+#rename original columns
+round_5.rename(columns={'Slot_x':'Slot', 'Tournament_x':'Tournament'}, inplace=True)
+#drop columns from merge
+round_5.drop(['Slot_y'], axis=1, inplace=True)
+
+#drop duplicates
+round_5.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+round_5 = round_5.merge(round_4_results, left_on=['WeakSeed','Tournament'], right_on=['Slot','Tournament'], how='left')
+#rename original columns
+round_5.rename(columns={'Slot_x':'Slot', 'Tournament_x':'Tournament'}, inplace=True)
+#drop columns from merge
+round_5.drop(['Slot_y'], axis=1, inplace=True)
+
+#drop duplicates
+round_5.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+def compare_seeds(seed1, seed2):
+    seed_num1 = extract_seed_number(seed1)
+    seed_num2 = extract_seed_number(seed2)
+
+    if seed_num1 == seed_num2:
+        # If the seed numbers are equal, compare the letters
+        return seed1 if seed1 < seed2 else seed2
+    else:
+        # Otherwise, compare the seed numbers
+        return seed1 if seed_num1 < seed_num2 else seed2
+
+# Apply comparison logic to determine StrongSeed and WeakSeed
+round_5['StrongSeed'] = round_5.apply(lambda x: compare_seeds(x['Team_x'], x['Team_y']), axis=1)
+round_5['WeakSeed'] = round_5.apply(lambda x: x['Team_x'] if x['Team_x'] != x['StrongSeed'] else x['Team_y'], axis=1)
+
+#drop Team_x and Team_y
+round_5.drop(['Team_x', 'Team_y'], axis=1, inplace=True)
+round_5
+
+
+round_5 = round_5.merge(preds_df, left_on=['StrongSeed','WeakSeed','Tournament'], right_on=['HigherSeedID','LowerSeedID','Tournament'], how='left')
+
+round_5['outcome'] = round_5['Pred'].apply(simulate_matchup)
+
+round_5['Team'] = round_5.apply(lambda x: x['HigherSeedID'] if x['outcome'] == True else x['LowerSeedID'], axis=1)
+
+round_5
+
+round_5_results = round_5[['Tournament','Slot','Team','ID']]
+
+round_5_results
+
+
+round_6 = tourney_2023[tourney_2023['Round'] == 6]
+
+round_6 = round_6.merge(round_5_results, left_on=['StrongSeed','Tournament'], right_on=['Slot','Tournament'], how='left')
+#rename original columns
+round_6.rename(columns={'Slot_x':'Slot', 'Tournament_x':'Tournament'}, inplace=True)
+#drop columns from merge
+round_6.drop(['Slot_y'], axis=1, inplace=True)
+
+#drop duplicates
+round_6.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+round_6 = round_6.merge(round_5_results, left_on=['WeakSeed','Tournament'], right_on=['Slot','Tournament'], how='left')
+#rename original columns
+round_6.rename(columns={'Slot_x':'Slot', 'Tournament_x':'Tournament'}, inplace=True)    
+#drop columns from merge
+round_6.drop(['Slot_y'], axis=1, inplace=True)
+
+#drop duplicates
+round_6.drop_duplicates(subset=['Slot','Tournament'], inplace=True)
+
+# Apply comparison logic to determine StrongSeed and WeakSeed
+round_6['StrongSeed'] = round_6.apply(lambda x: compare_seeds(x['Team_x'], x['Team_y']), axis=1)
+round_6['WeakSeed'] = round_6.apply(lambda x: x['Team_x'] if x['Team_x'] != x['StrongSeed'] else x['Team_y'], axis=1)
+
+round_6.drop(['Team_x', 'Team_y'], axis=1, inplace=True)
+
+round_6 = round_6.merge(preds_df, left_on=['StrongSeed','WeakSeed','Tournament'], right_on=['HigherSeedID','LowerSeedID','Tournament'], how='left')
+
+round_6['outcome'] = round_6['Pred'].apply(simulate_matchup)
+
+round_6['Team'] = round_6.apply(lambda x: x['HigherSeedID'] if x['outcome'] == True else x['LowerSeedID'], axis=1)
+
+round_6
+
+round_6_results = round_6[['Tournament','Slot','Team','ID']]
+
+
+all_rounds = pd.concat([round_1_results, round_2_results, round_3_results, round_4_results, round_5_results, round_6_results])
+
+all_rounds
+
+round_6_results
+
+
+#sample_submission as template for the output
+
+df = sample_submission.copy()
+
+#drop Team column from sample_submission
+df.drop('Team', axis=1, inplace=True)
+
+#merge sample_submission with all_rounds on Tournament and Slot
+submission = df.merge(all_rounds, left_on=['Tournament', 'Slot'], right_on=['Tournament', 'Slot'], how='left')
+
+#drop ID from submission
+submission.drop('ID', axis=1, inplace=True)
+
+submission
+
+#return all null rows from submission
+submission[submission['Team'].isnull()]
