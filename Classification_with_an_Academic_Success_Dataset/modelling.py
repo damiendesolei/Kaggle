f9e0175a42b8d4e292127a61669d82233d75c136
Runjie Zhu <damiendesolei@hotmail.com>
2024-06-02 00:16:19 +1000

    Create modelling.py
    submission_20240602_1

---
+++ Classification_with_an_Academic_Success_Dataset/modelling.py
@@ -0,0 +1,267 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  1 12:52:59 2024
+
+@author: zrj-desktop
+"""
+
+import datetime
+from colorama import Fore, Style
+import matplotlib.pyplot as plt
+
+import pandas as pd
+pd.set_option('display.max_columns', None)
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.ticker import MaxNLocator
+
+from sklearn.base import clone
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+
+
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, StratifiedKFold
+
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+import xgboost
+import catboost
+import lightgbm
+
+path = 'G:\kaggle\Classification_with_an_Academic_Success_Dataset\\'
+
+
+train = pd.read_csv(path + 'train.csv', index_col='id', low_memory=True)
+test = pd.read_csv(path + 'test.csv', index_col='id', low_memory=True)
+
+
+
+#features
+initial_features = list(test.columns)
+cat_features = ['Marital status', 'Application mode', 'Course',
+                'Previous qualification', 'Nacionality', "Mother's qualification", 
+                "Father's qualification", "Mother's occupation",
+                "Father's occupation"]
+for feature in cat_features:
+    for df in [train, test]:
+        df[feature] = df[feature].astype('category')
+
+#lable encode the target
+label_encoder = LabelEncoder()
+targets = label_encoder.fit_transform(train.Target)
+mapping = {index: label for index, label in enumerate(label_encoder.classes_)}
+for label, index in mapping.items():
+    print(f"{label}: {index}")
+
+#feature exploration
+dt = DecisionTreeClassifier(max_depth=5)
+dt.fit(train[initial_features], train.Target);
+
+
+plt.figure(figsize=(16, 6))
+plot_tree(dt, feature_names=initial_features, class_names=label_encoder.classes_, fontsize=7, impurity=True, filled=True, ax=plt.gca())
+plt.show()
+
+
+importance_df = pd.DataFrame({'feature': initial_features, 'importance': dt.feature_importances_})
+importance_df = importance_df.sort_values(by='importance', ascending=False)
+plt.figure(figsize=(10, 6))
+plt.barh(importance_df['feature'], importance_df['importance'], color='skyblue')
+plt.xlabel('Feature Importance')
+plt.ylabel('Feature')
+plt.title('Feature Importance in Decision Tree')
+plt.gca().invert_yaxis()
+plt.show()
+
+
+#plot top6 feature
+top_features = importance_df.head(6)['feature']
+
+plt.figure(figsize=(20, 24))
+plotnumber = 1
+
+# Loop through each column
+for col in top_features:
+    if plotnumber <= len(top_features) and train[col].dtype != 'float64':
+        plt.subplot(4, 3, plotnumber)
+        ax = sns.countplot(x=train[col], hue=train['Target'], palette='bright')
+        
+    plotnumber += 1
+
+plt.suptitle('Distribution of Categorical Variables by Target', fontsize=40, y=1)
+plt.tight_layout()
+plt.show()
+
+
+#integer encode: Target column
+# encode_target = {
+#     'Graduate': 0,
+#     'Dropout': 1,
+#     'Enrolled': 2
+# }
+# train['Target'] = train['Target'].map(encode_target)
+
+#set aside holdout sets for fitting ensemble weight
+train, holdout = train_test_split(train, test_size=0.1, stratify=train.Target , random_state = 24)
+
+
+#target distribution
+#train['Target'].describe()
+plt.hist(train['Target'],facecolor='skyblue', edgecolor='white')
+plt.ylabel('Count')
+plt.xlabel('Target')
+plt.show()
+
+
+#train vs test feature distribution
+_, axs = plt.subplots(12, 3, figsize=(12, 36))
+for col, ax in zip(initial_features, axs.ravel()):
+    vc = train[col].value_counts() / len(train)
+    ax.bar(vc.index, vc)
+    vc = test[col].value_counts() / len(test)
+    ax.bar(vc.index, vc, alpha=0.6)
+    ax.set_title(col)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True)) # only integer labels
+plt.tight_layout()
+plt.show()
+
+
+#correlation matrix
+plt.figure(figsize=(20, 16))
+plt.title('Correlation Matrix\n')
+sns.heatmap(train[initial_features].corr(), vmin=-1, vmax=1, annot=True, fmt='.2f', cmap='coolwarm', center=0)
+plt.show()
+
+
+
+#modelling
+#kf = KFold(n_splits=5, shuffle=True, random_state=24)
+kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=24)
+
+def cross_validate(model, label, features=initial_features):
+    """Compute out-of-fold and test predictions for a given model.
+    
+    Out-of-fold and test predictions are stored in the global variables
+    oof and test_pred, respectively.
+    
+    If n_repeats > 1, the model is trained several times with different seeds.
+    """
+    start_time = datetime.datetime.now()
+    scores = []
+    oof_preds = np.full_like(targets, np.nan, dtype=int)
+    #for fold, (idx_tr, idx_va) in enumerate(kf.split(train)):
+    for fold, (idx_tr, idx_va) in enumerate(kf.split(train, train.Target)):    
+        X_tr = train.iloc[idx_tr][features]
+        X_va = train.iloc[idx_va][features]
+        y_tr = targets[idx_tr]
+        y_va = targets[idx_va]
+        
+        model.fit(X_tr, y_tr)
+        y_pred = model.predict_proba(X_va)
+        y_predicted = np.argmax(y_pred, axis=1) #find the highest probability row array position 
+        
+        score = accuracy_score(y_va, y_predicted)
+        print(f"# Fold {fold}: accuracy={score:.5f}")
+        scores.append(score)
+        oof_preds[idx_va] = y_predicted #each iteration will fill in 1/5 of the index
+        
+    elapsed_time = datetime.datetime.now() - start_time
+    print(f"{Fore.GREEN}# Overall: {np.array(scores).mean():.5f} {label}"
+          f"   {int(np.round(elapsed_time.total_seconds() / 60))} min{Style.RESET_ALL}")
+    oof[label] = oof_preds
+    
+    if COMPUTE_TEST_PRED:
+        X_tr = train[features]
+        y_tr = targets
+        model.fit(X_tr, y_tr)
+        y_pred = model.predict_proba(test[features])
+        y_predicted = np.argmax(y_pred, axis=1)
+        test_pred[label] = y_predicted
+    
+    if COMPUTE_HOLDOUT_PRED:
+        X_tr = train[features]
+        y_tr = targets
+        model.fit(X_tr, y_tr)
+        y_pred = model.predict_proba(holdout[features])
+        y_predicted = np.argmax(y_pred, axis=1)
+        holdout_pred[label] = y_predicted
+
+
+# want to see the cross-validation results)
+COMPUTE_TEST_PRED = True
+COMPUTE_HOLDOUT_PRED = False
+
+# Containers for results
+oof, test_pred, holdout_pred = {}, {}, {}
+
+
+
+
+
+
+
+#xgboost
+model = xgboost.XGBClassifier(enable_categorical=True)
+cross_validate(model, 'Xgboost untuned', features=initial_features)
+
+#catboost
+model = catboost.CatBoostClassifier(cat_features=cat_features,verbose=False)
+cross_validate(model, 'Catboost untuned', features=initial_features)
+
+
+
+
+#Evaluation model results
+result_list = []
+for label in oof.keys():
+    mask = np.isfinite(oof[label])
+    score = accuracy_score(targets[mask], oof[label][mask])
+    result_list.append((label, score))
+result_df = pd.DataFrame(result_list, columns=['label', 'score'])
+result_df.sort_values('score', inplace=True, ascending=False)
+
+plt.figure(figsize=(12, len(result_df) * 0.4 + 0.4))
+bars = plt.barh(np.arange(len(result_df)), result_df.score, color='lightgreen')
+# plt.annotate('Best model without feature engineering', 
+#              (0.850, list(result_df.label).index('CatBoost')),
+#              xytext=(0.855, list(result_df.label).index('CatBoost')+2),
+#              arrowprops={'width': 2, 'color': 'darkgreen'},
+#              color='darkgreen')
+plt.gca().bar_label(bars, fmt='%.5f')
+plt.yticks(np.arange(len(result_df)), result_df.label)
+plt.gca().invert_yaxis()
+plt.xlim(0.825, 0.875)
+SINGLE_FOLD = False
+plt.xlabel(f'{"fold 0" if SINGLE_FOLD else "5-fold cv"} accuracy_score (higher is better)')
+plt.show()
+
+
+
+
+
+#submission 
+submission = pd.DataFrame(test_pred)
+submission['Target'] = label_encoder.inverse_transform(submission['Catboost untuned'])
+
+
+submission['id'] = test.index
+submission = submission[['id','Target']]
+#create submission file
+submission.to_csv(path+'submission_20240602_1.csv', index=False)
+
+
+
+
+#catboost untuned
+model = catboost.CatBoostClassifier(cat_features=cat_features)
+y = train['Target']
+col = initial_features
+X_train, X_val, y_train, y_val = train_test_split(train[col], y, test_size=0.4, random_state = 1024)
+model.fit(X_train, y_train)
+y_pred = model.predict_proba(X_val)
+accuracy_score(y_val, y_pred)
