3f649d9fea667c3af1c23a84735f0fb53886b378
Runjie Zhu <damiendesolei@hotmail.com>
2024-07-14 16:19:04 +1000

    Create modelling.py

---
+++ RSNA_2024_Lumbar_Spine_Degenerative_Classification/modelling.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pandas as pd
+import os
+import time
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+import pydicom as dicom
+import pydicom
+import json
+import glob
+import collections
+from pydicom.pixel_data_handlers.util import apply_voi_lut
+import cv2
+from skimage import measure
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import plotly.graph_objects as go
+import random
+from glob import glob
