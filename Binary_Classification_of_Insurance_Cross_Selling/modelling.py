72dfd682cbe874d971a4f7c69f301166bd47dd98
Runjie Zhu <damiendesolei@hotmail.com>
2024-07-24 23:31:04 +1000

    Update modelling.py
    set up mean encoding functions

--- Binary_Classification_of_Insurance_Cross_Selling/modelling.py
+++ Binary_Classification_of_Insurance_Cross_Selling/modelling.py
@@ -31,6 +31,8 @@ import datetime
 from colorama import Fore, Style
 
 
+import seaborn as sns
+
 
 ##############################################
 ######### read in data #######################
@@ -383,3 +385,81 @@ plt.show()
 
 ##############################################
 ######### feature probing - EDA ##############
+sns.kdeplot(data=train, x='Vehicle_Damage_encoded', hue='Response');
+sns.countplot(data=train, x='Response', hue='Vehicle_Damage_encoded') #train[train.Vehicle_Damage_encoded==0].Response.value_counts(normalize=True)
+
+sns.kdeplot(data=train, x='Age', hue='Response');
+sns.countplot(data=train, x='Response', hue='Age')
+
+sns.kdeplot(data=train, x='Previously_Insured', hue='Response');
+sns.countplot(data=train, x='Response', hue='Previously_Insured')
+
+sns.kdeplot(data=train, x='Policy_Sales_Channel', hue='Response');
+sns.countplot(data=train, x='Response', hue='Policy_Sales_Channel')
+
+sns.kdeplot(data=train, x='Vintage', hue='Response');
+sns.kdeplot(data=train, x='Vehicle_Age_encoded', hue='Response');
+sns.kdeplot(data=train, x='Annual_Premium', hue='Response');
+sns.kdeplot(data=train, x='Region_Code', hue='Response');
+
+sns.kdeplot(data=train, x='Gender_encoded', hue='Response');
+sns.countplot(data=train, x='Response', hue='Gender_encoded')
+
+sns.kdeplot(data=train, x='Driving_License', hue='Response');
+sns.kdeplot(data=train, x='random', hue='Response');
+######### End of feature probing EDA #########
+##############################################
+
+
+
+
+##############################################
+######### target encoding features ###########
+def add_noise(series, noise_level):
+    return series * (1 + noise_level * np.random.randn(len(series)))
+
+def target_encode(trn_series=None, 
+                  tst_series=None, 
+                  target=None, 
+                  min_samples_leaf=1, 
+                  smoothing=1,
+                  noise_level=0):
+    """
+    Smoothing is computed like in the following paper by Daniele Micci-Barreca
+    https://kaggle2.blob.core.windows.net/forum-message-attachments/225952/7441/high%20cardinality%20categoricals.pdf
+    trn_series : training categorical feature as a pd.Series
+    tst_series : test categorical feature as a pd.Series
+    target : target data as a pd.Series
+    min_samples_leaf (int) : minimum samples to take category average into account
+    smoothing (int) : smoothing effect to balance categorical average vs prior  
+    """ 
+    assert len(trn_series) == len(target)
+    assert trn_series.name == tst_series.name
+    temp = pd.concat([trn_series, target], axis=1)
+    # Compute target mean 
+    averages = temp.groupby(by=trn_series.name)[target.name].agg(["mean", "count"])
+    # Compute smoothing
+    smoothing = 1 / (1 + np.exp(-(averages["count"] - min_samples_leaf) / smoothing))
+    # Apply average function to all target data
+    prior = target.mean()
+    # The bigger the count the less full_avg is taken into account
+    averages[target.name] = prior * (1 - smoothing) + averages["mean"] * smoothing
+    averages.drop(["mean", "count"], axis=1, inplace=True)
+    # Apply averages to trn and tst series
+    ft_trn_series = pd.merge(
+        trn_series.to_frame(trn_series.name),
+        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
+        on=trn_series.name,
+        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
+    # pd.merge does not keep the index so restore it
+    ft_trn_series.index = trn_series.index 
+    ft_tst_series = pd.merge(
+        tst_series.to_frame(tst_series.name),
+        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
+        on=tst_series.name,
+        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
+    # pd.merge does not keep the index so restore it
+    ft_tst_series.index = tst_series.index
+    return add_noise(ft_trn_series, noise_level), add_noise(ft_tst_series, noise_level)
+
+#1.Age
