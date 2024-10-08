52d8257064e63ab9d7207fbbc45c50fa78290398
Runjie Zhu <damiendesolei@hotmail.com>
2024-10-08 21:45:52 +1100

    Update modelling.py

--- Child_Mind_Institute_Problematic_Internet_Use/modelling.py
+++ Child_Mind_Institute_Problematic_Internet_Use/modelling.py
@@ -5,22 +5,65 @@ Created on Sun Oct  6 16:24:10 2024
 @author: damie
 """
 
+
+
 import numpy as np
 import pandas as pd
 
+
+import lightgbm as lgb
+from catboost import CatBoostRegressor, CatBoostClassifier
+from xgboost import XGBRegressor
+from sklearn.ensemble import VotingRegressor
 from sklearn.model_selection import *
 from sklearn.metrics import *
 
+
 from tqdm import tqdm
+import os
+from concurrent.futures import ThreadPoolExecutor
 
 
 
-#PATH = 'G:\kaggle\Classification_with_an_Academic_Success_Dataset\\'
-PATH = r'C:\Users\damie\Downloads\child-mind-institute-problematic-internet-use\\'
+
+
+#https://www.kaggle.com/code/abdmental01/cmi-best-single-model
+def process_file(dirname, filename):
+    df = pd.read_parquet(os.path.join(dirname, filename, 'part-0.parquet'))
+    df.drop('step', axis=1, inplace=True)
+    return df.describe().values.reshape(-1), filename.split('=')[1]
+
+def load_time_series(dirname) -> pd.DataFrame:
+    ids = os.listdir(dirname)
+    
+    with ThreadPoolExecutor() as executor:
+        results = list(tqdm(executor.map(lambda fname: process_file(dirname, fname), ids), total=len(ids)))
+    
+    stats, indexes = zip(*results)
+    
+    df = pd.DataFrame(stats, columns=[f"Stat_{i}" for i in range(len(stats[0]))])
+    df['id'] = indexes
+    
+    return df
+
+
+#load the data
+
+#PATH = r'C:\Users\damie\Downloads\child-mind-institute-problematic-internet-use\\'
+PATH = r'G:\kaggle\child-mind-institute-problematic-internet-use\\'
 
 train = pd.read_csv(PATH + 'train.csv', index_col='id', low_memory=True)
 test = pd.read_csv(PATH + 'test.csv', index_col='id', low_memory=True)
 data_dict = pd.read_csv(PATH + 'data_dictionary.csv', low_memory=True)
+sample = pd.read_csv(PATH + 'sample_submission.csv')
+
+train_ts = load_time_series(PATH + 'series_train.parquet')
+test_ts = load_time_series(PATH + 'series_test.parquet')
+time_series_cols = train_ts.columns.tolist()
+time_series_cols.remove("id")
+
+
+
 
 train.info(memory_usage='deep')
 
@@ -36,24 +79,6 @@ pciat_min_max
 data_dict[data_dict['Field'] == 'PCIAT-PCIAT_Total']['Value Labels'].iloc[0]
 
 
-#https://www.kaggle.com/code/abdmental01/cmi-best-single-model
-def process_file(filename, dirname):
-    df = pd.read_parquet(os.path.join(dirname, filename, 'part-0.parquet'))
-    df.drop('step', axis=1, inplace=True)
-    return df.describe().values.reshape(-1), filename.split('=')[1]
-
-def load_time_series(dirname) -> pd.DataFrame:
-    ids = os.listdir(dirname)
-    
-    with ThreadPoolExecutor() as executor:
-        results = list(tqdm(executor.map(lambda fname: process_file(fname, dirname), ids), total=len(ids)))
-    
-    stats, indexes = zip(*results)
-    
-    df = pd.DataFrame(stats, columns=[f"Stat_{i}" for i in range(len(stats[0]))])
-    df['id'] = indexes
-    
-    return df
 
 
 def quadratic_weighted_kappa(y_true, y_pred):
