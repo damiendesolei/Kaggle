0f5f09fc572c421173a91461e6cfcf5541ac7647
Runjie Zhu <damiendesolei@hotmail.com>
2025-02-02 12:12:18 +1100

    Update ensemble_stacking.py
    setup glm with logit link

--- exploring-predictive-health-factors/ensemble_stacking.py
+++ exploring-predictive-health-factors/ensemble_stacking.py
@@ -6,6 +6,7 @@ Created on Sat Feb  1 16:59:09 2025
 """
 
 import pandas as pd
+import numpy as np
 import statsmodels.api as sm
 
 
@@ -13,23 +14,50 @@ PATH = r'G:\\kaggle\exploring-predictive-health-factors\model\ensemble\\'
 
 
 # Read in model predictions
-# PCOS_Single_XGB_4 - Version 1
-pred_xgb = pd.read_csv(PATH + 'xgb_12_parameters_submission_cv_auc_0.8587_LB_0.916.csv')
-# cat_12_parameters_submission_cv_auc_0.8421.csv
-pred_cat = pd.read_csv(PATH + 'cat_12_parameters_submission_cv_auc_0.8421_LB_0.900.csv')
-# PCOS_Single_LGB_2 - Version 2
-pred_lgb = pd.read_csv(PATH + 'lgb_12_parameters_submission_cv_auc_0.8227_LB_0.923.csv')
+# PCOS_Single_XGB_4 - Version 7
+pred_xgb = pd.read_csv(PATH + 'xgb_12_parameters_reserved_cv_auc_0.8587.csv')
+# PCOS_Single_CAT_2 - Version 4
+pred_cat = pd.read_csv(PATH + 'cat_12_parameters_reserved_cv_auc_0.8683.csv')
+# PCOS_Single_LGB_5 - Version 4
+pred_lgb = pd.read_csv(PATH + 'lgb_12_parameters_reserved_cv_auc_0.8560.csv')
 
 
 
 # Define independent variables (exclude intercept)
-X = data[['feature1', 'feature2', 'feature3']]
-y = data['target']
+X = pd.DataFrame()
+X['pred_xgb'] = pred_xgb['pred_xgb']
+X['pred_cat'] = pred_cat['pred_lgb'] #typo to fix
+X['pred_lgb'] = pred_lgb['pred_lgb']
+y = pred_xgb['PCOS']
+
+# Add an intercept column
+X = sm.add_constant(X)  # Adds a column of 1s for the intercept
 
 # Fit a GLM with a logit link function and NO intercept
 glm_model = sm.GLM(y, X, family=sm.families.Binomial(link=sm.families.links.logit()))
-result = glm_model.fit()
+glm_fitted = glm_model.fit()
 
-# Print coefficients explicitly
-print("GLM Coefficients (No Intercept):")
-print(result.params)
+
+
+
+# Read in submission files
+submission_xgb = pd.read_csv(PATH + 'xgb_12_parameters_submission_cv_auc_0.8587_LB_0.916.csv')
+submission_cgb = pd.read_csv(PATH + 'cat_12_parameters_submission_cv_auc_0.8683_LB_0.903.csv')
+submission_lgb = pd.read_csv(PATH + 'lgb_12_parameters_submission_cv_auc_0.8560_LB_0.923.csv')
+
+X_ = pd.DataFrame()
+X_['pred_xgb'] = submission_xgb['PCOS']
+X_['pred_cat'] = submission_cgb['PCOS'] 
+X_['pred_lgb'] = submission_lgb['PCOS']
+# Add an intercept column
+X_ = sm.add_constant(X_)  # Adds a column of 1s for the intercept
+
+y_submission = glm_fitted.predict(X_)
+
+
+# Ensemble
+submission = submission_lgb.copy()
+submission['PCOS'] = y_submission
+submission.to_csv(PATH+'lgb_0.923_xgb_0.916_cat_0.903_blend_1.csv', index=False)
+
+
