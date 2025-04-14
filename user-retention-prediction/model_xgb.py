00ddfb02aecbb581a35348a94bd824097806a257
Runjie Zhu <damiendesolei@hotmail.com>
2025-04-15 00:05:09 +1000

    Update model_xgb.py
    update to initial features

--- user-retention-prediction/model_xgb.py
+++ user-retention-prediction/model_xgb.py
@@ -5,49 +5,68 @@ Created on Sat Mar 22 12:08:21 2025
 @author: zrj-desktop
 """
 
-import polars as pl
+import warnings
+warnings.simplefilter('ignore')
+import os
+import gc
 import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 100)
+from tqdm import tqdm
+from sklearn.model_selection import KFold
+import matplotlib.pyplot as plt
+import seaborn as sns
+#from gensim.models import Word2Vec
+import lightgbm as lgb
+
+
 
 PATH = r'G:\\kaggle\user-retention-prediction\\'
 
 
-train=pl.read_csv(PATH+"train.csv").with_columns([
-    ((1540051199-pl.col("Timestamp"))//(24*60*60)).alias("days")
-])  # days prior to 2018-10-20 23:59:59
-sub=pl.read_csv(PATH+"submit_sample.csv")
+
+# load the 
+train = pd.read_csv(PATH+ 'train.csv')
+
+train['DateTime'] = pd.to_datetime(train['Timestamp'], unit='s')
+
+## timezone correction +8h 
+train['DateTime'] = train['DateTime'] + pd.Timedelta(hours=8)#'2018-09-21 00:00:00' to '2018-10-20 23:59:59' 
+
+# create month-day combination
+train['MonthDay'] = train['DateTime'].dt.month * 100 + train['DateTime'].dt.day
+
+train.tail()
 
 
 
-last_time=train.group_by("ID").agg((1540051199-pl.col("Timestamp").max()).alias("lasttime"))
+# training set
+train_df = train[(train['DateTime'] >= '2018-09-21 00:00:00')&\
+                 (train['DateTime'] <= '2018-10-13 23:59:59')].reset_index(drop=True)
+    
+# active on the last day of training set
+active_ids = train_df[(train_df['DateTime'] >= '2018-10-13 00:00:00')&\
+                     (train_df['DateTime'] <= '2018-10-13 23:59:59')]['ID'].unique().tolist()
+# validation set
+valid_df = train[(train['DateTime'] >= '2018-10-14 00:00:00')&\
+              (train['DateTime'] <= '2018-10-20 23:59:59')].reset_index(drop=True)
+    
+# active is there is any action on 2018-10-13
+active_df = pd.DataFrame({'ID': active_ids})
+mapping = valid_df.groupby('ID')['MonthDay'].nunique().to_dict()
+active_df['label'] = active_df['ID'].map(mapping)
+active_df['label'].fillna(0, inplace=True)
+active_df['label'] = active_df['label'].astype(int)
 
-sub_last_time=sub.join(last_time,on="ID",how="left").fill_null(2*24*3600)
-sub_last_time["lasttime"].max()/24/3600
-
-
-
-# Finds IDs that logged in within 7 days and their number of days logged in
-label=train.filter(pl.col("days")==7)[["ID"]].unique().join(  
-    train.filter(pl.col("days")<7).group_by(["ID"]).agg([
-    pl.col("days").n_unique().cast(int).alias("label")
-]),on="ID",how="left").fill_null(0)
-dist=label.group_by("label").agg((pl.col("ID").count()/len(label)).alias("ratio")).sort("label")
-thresholds=np.cumsum(dist["ratio"].to_list()) 
-
-
-
-shift_days=7
-
-# possible features
-## average hours online
-## max hours online (last action time - first action time)
-## max consecutive days logged in
-## certain action count
-## consecutive days of certain action or combination of actions
-## holidays vs weekdays
-## login time of the day (e.g. morning, afternoon, evening)
+active_df['label'].value_counts(normalize=True, dropna=False)
 
 
-features=train.filter(pl.col("days")==shift_days)[["ID"]].unique().join(
-    train.filter(pl.col("days").is_in([shift_days+i for i in range(7)])).group_by(["ID","days"]).agg([
-    pl.col("ActionType").count().alias("action_count"),
-]),on="ID",how="left").fill_null(0)
+### Features ###
+def action_feature(train_df, valid_df):
+    
+    df = valid_df.copy()
+    
+    # Number of actions per ID
+    mapping = train_df.groupby(['ID'])['ID'].count().to_dict()
+    df['total_actions'] = df['ID'].map(mapping)
