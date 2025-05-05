40bf81ab91c4035682cddf1825a8194656290c86
Runjie Zhu <damiendesolei@hotmail.com>
2025-05-05 21:44:45 +1000

    Update 5.catboost_classifier.py
    small changes

--- user-retention-prediction/5.catboost_classifier.py
+++ user-retention-prediction/5.catboost_classifier.py
@@ -120,6 +120,52 @@ def multiclass_logloss(y_actual, y_pred):
 FEATURES = features_name
 train = train_data.to_pandas()
 
+# def reduce_mem_usage(df, float16_as32=True):
+#     #memory_usage() to sum all ram for each column, B->KB->MB
+#     start_mem = df.memory_usage().sum() / 1024**2
+#     print('Memory usage of current dataframe is {:.2f} MB'.format(start_mem))
+#     non_date_columns = [col for col in df.columns if df[col].dtype != 'datetime64[ns]']
+
+#     for col in non_date_columns:
+#         col_type = df[col].dtype 
+#         if col_type != object and str(col_type)!='category':#不是object也就是说这里处理的是数值类型的变量
+#             c_min,c_max = df[col].min(),df[col].max() #求出这列的最大值和最小值
+#             if str(col_type)[:3] == 'int':#如果是int类型的变量,不管是int8,int16,int32还是int64
+#                 #如果这列的取值范围是在int8的取值范围内,那就对类型进行转换 (-128 到 127)
+#                 if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+#                     df[col] = df[col].astype(np.int8)
+#                 #如果这列的取值范围是在int16的取值范围内,那就对类型进行转换(-32,768 到 32,767)
+#                 elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+#                     df[col] = df[col].astype(np.int16)
+#                 #如果这列的取值范围是在int32的取值范围内,那就对类型进行转换(-2,147,483,648到2,147,483,647)
+#                 elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+#                     df[col] = df[col].astype(np.int32)
+#                 #如果这列的取值范围是在int64的取值范围内,那就对类型进行转换(-9,223,372,036,854,775,808到9,223,372,036,854,775,807)
+#                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+#                     df[col] = df[col].astype(np.int64)  
+#             else:#如果是浮点数类型.
+#                 #如果数值在float16的取值范围内,如果觉得需要更高精度可以考虑float32
+#                 if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+#                     if float16_as32:#如果数据需要更高的精度可以选择float32
+#                         df[col] = df[col].astype(np.float32)
+#                     else:
+#                         df[col] = df[col].astype(np.float16)  
+#                 #如果数值在float32的取值范围内，对它进行类型转换
+#                 elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+#                     df[col] = df[col].astype(np.float32)
+#                 #如果数值在float64的取值范围内，对它进行类型转换
+#                 else:
+#                     df[col] = df[col].astype(np.float64)
+#     #current ram
+#     end_mem = df.memory_usage().sum() / 1024**2
+#     print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+#     #ram reduction%
+#     print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+#     return df
+
+# train = reduce_mem_usage(train)
+
 # Define the parameter space
 def objective(trial):
     
@@ -145,7 +191,7 @@ def objective(trial):
     }
 
     # Time series cross-validation
-    skf = StratifiedKFold(n_splits=4, shuffle=False)
+    skf = StratifiedKFold(n_splits=3, shuffle=False)
     scores = []
     
     for i, (train_index, test_index) in enumerate(skf.split(train, train["label"])):       
